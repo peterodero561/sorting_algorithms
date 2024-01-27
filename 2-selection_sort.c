f61ef17f6cda1e6c06a8056b630fc8d6098b10ad
Peter Odero <peterodero561@gmail.com>
2024-01-27 04:37:49 +0000

    perf: all: changed the style of code

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,20 +24,26 @@ void swap(int *xp, int *yp)
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
+	int temp;
 
 	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[min_idx] > array[j])
+			if (array[j] < array[min_idx])
 			{
 				min_idx = j;
 			}
 		}
-		swap(&array[i], &array[min_idx]);
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+		}
 		print_array(array, size);
 	}
 }
