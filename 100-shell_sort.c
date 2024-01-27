b2ab5c49aae56fb60c3da1afd36b9297a150cdb5
Peter Odero <peterodero561@gmail.com>
2024-01-27 05:46:14 +0000

    fix:line 14 & 15: trailing spaces

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,8 +11,9 @@ void shell_sort(int *array, size_t size)
 {
 	size_t i, j, gap;
 	int temp;
-	
-	for (gap = 1; gap < size / 3; gap = gap * 3 + 1);
+
+	for (gap = 1; gap < size / 3; gap = gap * 3 + 1)
+		;
 	for (; gap > 0; gap /= 3)
 	{
 		for (i = gap; i < size; i++)
